1e0f072cae3378287d660f9e393665e91344ad41
claasahl <claasahl@gmail.com>
2015-01-14 19:14:45 +0100

    fixed tests

--- src/main/java/de/claas/mosis/io/FileHandler.java
+++ src/main/java/de/claas/mosis/io/FileHandler.java
@@ -4,6 +4,7 @@ import de.claas.mosis.annotation.Parameter;
 import de.claas.mosis.model.Condition;
 
 import java.io.File;
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Stack;
@@ -12,10 +13,8 @@ import java.util.Stack;
  * The class {@link de.claas.mosis.io.FileHandler}. It is intended to enable
  * enumeration of file system objects (such aus files and directories). This
  * {@link de.claas.mosis.io.DataHandler} provides an alternative to the
- * otherwise (mostly) stream-based communication.
- * <p/>
- * As opposed to other {@link de.claas.mosis.io.DataHandler} implementation,
- * this realization is read-only.
+ * otherwise (mostly) stream-based communication. When in {@link #MODE_WRITE},
+ * the corresponding files are created (if they do not already exist).
  *
  * @author Claas Ahlrichs (claasahl@tzi.de)
  */
@@ -31,8 +30,6 @@ public class FileHandler extends DataHandler<File> {
     public FileHandler() {
         addCondition(ROOT, new Condition.IsNotNull());
         setParameter(ROOT, File.listRoots()[0].getAbsolutePath());
-        addCondition(MODE, new Condition.IsInList(Arrays.asList(MODE_READ)));
-        setParameter(MODE, MODE_READ);
     }
 
     @Override
@@ -50,16 +47,31 @@ public class FileHandler extends DataHandler<File> {
 
     @Override
     public void process(List<File> in, List<File> out) {
-        if (!files.isEmpty()) {
-            while (!files.peek().exists()) {
-                files.pop();
-            }
-            File exists = files.pop();
-            out.add(exists);
+        if (isReadOnly(in)) {
+            if (!files.isEmpty()) {
+                while (!files.peek().exists()) {
+                    files.pop();
+                }
+                File exists = files.pop();
+                out.add(exists);
 
-            File[] list = exists.listFiles();
-            if (list != null) {
-                files.addAll(Arrays.asList(list));
+                File[] list = exists.listFiles();
+                if (list != null) {
+                    files.addAll(Arrays.asList(list));
+                }
+            }
+        } else {
+            for (File file : in) {
+                if (file != null) {
+                    try {
+                        file.createNewFile();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+            if (shouldForward()) {
+                out.addAll(in);
             }
         }
     }
--- src/test/java/de/claas/mosis/io/FileHandlerTest.java
+++ src/test/java/de/claas/mosis/io/FileHandlerTest.java
@@ -4,8 +4,9 @@ import de.claas.mosis.util.Utils;
 import org.junit.Test;
 
 import java.io.File;
+import java.util.Arrays;
 
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 /**
  * The JUnit test for class {@link de.claas.mosis.io.FileHandler}. It is
@@ -16,9 +17,28 @@ import static org.junit.Assert.assertNotNull;
  */
 public class FileHandlerTest extends DataHandlerTest<File, FileHandler> {
 
+    private final File tmp1 = new File("./" + FileHandler.class.getSimpleName() + "_1.tmp");
+    private final File tmp2 = new File("./" + FileHandler.class.getSimpleName() + "_2.tmp");
+    private final File tmp3 = new File("./" + FileHandler.class.getSimpleName() + "_3.tmp");
+
+    @Override
+    public void after() {
+        super.after();
+        tmp1.delete();
+        tmp2.delete();
+        tmp3.delete();
+    }
+
     @Override
     protected FileHandler build() throws Exception {
         return new FileHandler();
+    }
+
+    @Test
+    public void shouldNotExist() {
+        assertFalse(tmp1.exists());
+        assertFalse(tmp2.exists());
+        assertFalse(tmp3.exists());
     }
 
     @Override
@@ -31,15 +51,42 @@ public class FileHandlerTest extends DataHandlerTest<File, FileHandler> {
     }
 
     @Override
-    @Test(expected = IllegalArgumentException.class)
     public void shouldWrite() throws Exception {
         _H.setParameter(DataHandler.MODE, DataHandler.MODE_WRITE);
+
+        assertNull(Utils.process(_H, (File) null));
+        assertEquals(Arrays.asList(tmp1, tmp2), Utils.processAll(_H, tmp1, tmp2));
+        assertTrue(tmp1.exists());
+        assertTrue(tmp2.exists());
+        assertFalse(tmp3.exists());
+        assertEquals(tmp3, Utils.process(_H, tmp3));
+        assertTrue(tmp1.exists());
+        assertTrue(tmp2.exists());
+        assertTrue(tmp3.exists());
     }
 
     @Override
-    @Test(expected = IllegalArgumentException.class)
     public void shouldDetermineMode() throws Exception {
         _H.setParameter(DataHandler.MODE, DataHandler.MODE_AUTO);
+
+        assertNull(Utils.process(_H, (File) null));
+
+        assertEquals(Arrays.asList(tmp1, tmp2), Utils.processAll(_H, tmp1, tmp2));
+        assertTrue(tmp1.exists());
+        assertTrue(tmp2.exists());
+        assertFalse(tmp3.exists());
+
+        assertNotNull(Utils.process(_H));
+        assertNotNull(Utils.process(_H));
+        assertNotNull(Utils.process(_H));
+        assertTrue(tmp1.exists());
+        assertTrue(tmp2.exists());
+        assertFalse(tmp3.exists());
+
+        assertEquals(tmp3, Utils.process(_H, tmp3));
+        assertTrue(tmp1.exists());
+        assertTrue(tmp2.exists());
+        assertTrue(tmp3.exists());
     }
 
 }
